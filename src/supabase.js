62acf7eeec8e33ed57b00fc3cffa317eb6ec7b77
Carol-88 <gralleta@gmail.com>
2024-05-04 12:27:49 +0200

    prepare to first user storie

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'tu_supabase_url';
-const SUPABASE_ANON_KEY = 'tu_supabase_anon_key';
+const SUPABASE_URL = 'https://jzxwknpxvgtmuwqngkva.supabase.co';
+const SUPABASE_ANON_KEY = import.meta.SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
