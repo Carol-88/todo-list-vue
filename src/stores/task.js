d340c2f5cd585afd740ded68d94183d10f954cb6
Carol-88 <gralleta@gmail.com>
2024-05-08 14:58:40 +0200

    work in addtasks

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 import { supabase } from "../supabase";
- 
+
 export const useTaskStore = defineStore("tasks", {
   state: () => ({
     tasks: null,
@@ -8,10 +8,21 @@ export const useTaskStore = defineStore("tasks", {
   actions: {
     async fetchTasks() {
       const { data: tasks } = await supabase
-        .from("tasks")
-        .select("*")
-        .order("id", { ascending: false });
+       .from("tasks")
+       .select("*")
+       .order("id", { ascending: false });
       this.tasks = tasks;
+    },
+    async addTask(newTaskTitle) {
+      const { data, error } = await supabase
+      .from("tasks")
+      .insert([{ task: newTaskTitle }]);
+      if (error) {
+        console.error("Error al agregar la tarea:", error);
+        return;
+      }
+      // Actualizar el estado de las tareas en el store
+      this.fetchTasks(); // Esto actualizará el estado de tasks con las nuevas tareas
     },
   },
 });
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,10 +1,28 @@
-<script setup>
-import LogoutComponent from "@/components/LogoutComponent.vue";
-</script>
 <template>
   <section>
     <h1>dashboard</h1>
     <LogoutComponent />
+    <div>
+      <input v-model="newTaskTitle" type="text" placeholder="Nueva tarea" />
+      <button @click="addTask">Agregar tarea</button>
+    </div>
   </section>
 </template>
-<style scoped></style>
+
+<script setup>
+import LogoutComponent from "@/components/LogoutComponent.vue";
+import { ref } from "vue";
+import { useTaskStore } from "@/stores/task";
+
+const taskStore = useTaskStore();
+const newTaskTitle = ref("");
+
+const addTask = () => {
+  if (newTaskTitle.value.trim() === "") {
+    alert("Por favor, ingresa una tarea válida.");
+    return;
+  }
+  taskStore.addTask(newTaskTitle.value);
+  newTaskTitle.value = "";
+};
+</script>
