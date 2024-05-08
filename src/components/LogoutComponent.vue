5b20ab423508d6a201706e0a1829b3dd4ff3d3cb
Carol-88 <gralleta@gmail.com>
2024-05-08 13:06:05 +0200

    logout component

---
+++ src/components/LogoutComponent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="logout-container">
+    <button @click="handleLogout">Cerrar sesión</button>
+  </div>
+</template>
+
+<script setup>
+import { useUserStore } from "../stores/user.js";
+import { useRouter } from "vue-router";
+
+const userStore = useUserStore();
+const router = useRouter();
+
+const handleLogout = async () => {
+  try {
+    await userStore.signOut();
+
+    router.push({ path: "/auth" });
+  } catch (error) {
+    console.error("Error al cerrar sesión:", error.message);
+    alert("Error al cerrar sesión. Por favor, inténtalo de nuevo.");
+  }
+};
+</script>
+
+<style scoped>
+.logout-container {
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import LogoutComponent from "@/components/LogoutComponent.vue";
+</script>
 <template>
-    <section>
-        <h1>dashboard</h1>
-    </section>
+  <section>
+    <h1>dashboard</h1>
+    <LogoutComponent />
+  </section>
 </template>
 <style scoped></style>
